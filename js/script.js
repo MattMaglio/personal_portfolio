3004224cdb906d50487182bd55a9b65be11f030f
Matias Adrian Maglio <matiasadrian.maglio@gmail.com>
2024-09-22 01:43:54 -0300

    Modificacion sobre la manera que se envia el email

--- js/script.js
+++ js/script.js
@@ -18,11 +18,12 @@ logoImages.forEach(img => {
 
 document.getElementById('py-section').style.display = 'block';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const contactForm = document.querySelector('.contact-form');
+// Selecciona el formulario después de que el DOM ha cargado
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.querySelector('.contactForm');
 
     contactForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // Evita el envío tradicional del formulario
 
         // Obtener valores de los campos
         const name = document.getElementById('name').value.trim();
@@ -41,13 +42,39 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
-        /*alert('Formulario enviado con éxito.\nGracias por contactarte, ' + name + '!');*/
+        // Crear un objeto con los datos del formulario
+        const formData = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
+        };
 
+        // Enviar el formulario a través de fetch a FormSubmit
+        fetch('https://formsubmit.co/ajax/matiasadrian.maglio@gmail.com', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(formData) // Convierte los datos del formulario a JSON
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Mostrar un mensaje de éxito
+            alert('Formulario enviado con éxito.\nGracias por contactarte, ' + name + '!');
+            contactForm.reset(); // Resetea el formulario después de enviarlo
+        })
+        .catch(error => {
+            // Manejar errores en el envío
+            alert('Hubo un error al enviar el formulario. Por favor, intenta de nuevo.');
+            console.error('Error:', error);
+        });
     });
-
-    // Función para validar el formato del correo electrónico
-    function validateEmail(email) {
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(String(email).toLowerCase());
-    }
 });
+
+// Función para validar el formato del correo electrónico
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
